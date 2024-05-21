88cdc21dd50bc52960dee7323e8b694c05a463ae
Deepak Sheoran <sheorandeepak1999@gmail.com>
2024-05-21 12:22:28 +0530

    Initial Commit

---
+++ !00 Days of Code/Day 15/Coffee Machine/main.py
@@ -0,0 +1,8 @@
+"""
+About the program "Coffee Machine"
+This program allows the user to choose a type of coffee from three options. The coffee will then be prepared ny the
+machine and given to the user but while making the coffee, the program will deduct the amount of various resources
+used from the data it has since during the preparation of the coffee these resources will be used. Now, before the
+coffee is given to the user, the program will check what it costs and demand money and take it from the user.
+"""
+# TODO
