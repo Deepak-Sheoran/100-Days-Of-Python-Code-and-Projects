802bcb5fe12ae740301699185e0e2a6da2604874
Deepak Sheoran <sheorandeepak1999@gmail.com>
2024-05-25 20:49:17 +0530

    Initial Commit

--- !00 Days of Code/Day 16/practice OOP.py
+++ !00 Days of Code/Day 16/practice OOP.py
@@ -8,4 +8,3 @@ window = Screen()
 print(window.canvheight)  # Here 'canvheight' is an attribute associated with the object
 window.exitonclick()  # The exitonclick is a method associated with the object. Its functionality is to stop the window
 # from disappearing unless we click on it.
-
